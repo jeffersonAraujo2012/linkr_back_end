6cb01376141abc1950089cfae3009c5514ee2cf5
Lucas Dias <lucasdias.ld10@gmail.com>
2023-03-08 15:46:06 -0300

    delete post in progress

--- src/routers/posts.router.js
+++ src/routers/posts.router.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import createPost from '../controller/createPost.controller.js';
 import getPosts from '../controller/getPosts.controller.js';
-import { editPost } from '../controller/editPosts.controller.js';
+import { deletePost, editPost } from '../controller/posts.controller.js';
 const postsRouter = express.Router();
 
 postsRouter.get('/', getPosts);
 postsRouter.post('/', createPost);
-postsRouter.post('/edit', editPost)
+postsRouter.patch('/', editPost);
+postsRouter.delete('/', deletePost);
 
 export default postsRouter;
