45cb6daa40259d73fb42d2523dc850994ad7287a
luanmed <luanmede@gmail.com>
2023-03-08 15:11:35 -0300

    feat: search user by id or name

--- src/app.js
+++ src/app.js
@@ -2,13 +2,14 @@ import express from "express"
 import dotenv from "dotenv"
 import cors from "cors"
 import authRouter from "./routers/AuthRouter.js"
+import searchRouter from "./routers/SearchRouter.js"
 
 const app = express()
 dotenv.config()
 app.use(express.json())
 app.use(cors())
 
-app.use( [ authRouter ] )
+app.use( [ authRouter, searchRouter ] )
 
 const port = process.env.PORT || 5000
 app.listen(port, () => {
