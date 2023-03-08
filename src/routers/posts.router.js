0b13ec0785efec02423fd87c1ea9066727bddde7
Jefferson Araújo <jeffersonc201212@gmail.com>
2023-03-08 20:19:14 -0300

    Implemented: publish and timeline.

--- src/routers/posts.router.js
+++ src/routers/posts.router.js
@@ -2,10 +2,12 @@ import express from 'express';
 import createPost from '../controller/createPost.controller.js';
 import getPosts from '../controller/getPosts.controller.js';
 import { deletePost, editPost } from '../controller/posts.controller.js';
+import { validateSchema } from '../middlewares/validateSchema.js';
+import postSchema from '../schemas/post.schema.js';
 const postsRouter = express.Router();
 
 postsRouter.get('/', getPosts);
-postsRouter.post('/', createPost);
+postsRouter.post('/', validateSchema(postSchema),createPost);
 postsRouter.patch('/', editPost);
 postsRouter.delete('/', deletePost);
 
