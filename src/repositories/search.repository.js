39e8f2ea298d7d508507b76c4955924ac5fe5318
luanmed <luanmede@gmail.com>
2023-03-09 18:45:35 -0300

    fix: query searchUserById

--- src/repositories/search.repository.js
+++ src/repositories/search.repository.js
@@ -2,7 +2,7 @@ import { db } from "../config/database.js";
 
 export async function searchUserByName (name) {
     const user = await db.query(`
-    SELECT users.picture_url, users.username
+    SELECT users.picture_url, users.username, users.id
     FROM users WHERE username LIKE $1;`, [`${name}%`]);
 
     return user;
@@ -12,7 +12,7 @@ export async function searchUserById (id) {
     const user = await db.query(`
         SELECT posts.*, users.username, users.picture_url AS picture_user
         FROM posts
-        JOIN users
+        RIGHT JOIN users
         ON posts.user_id = users.id
 	    WHERE users.id = $1
         ORDER BY created_at DESC
