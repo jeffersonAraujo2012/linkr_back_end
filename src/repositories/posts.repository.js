9294a9344eb321ae8094b1201411ccc6ad212e0f
Lucas Dias <lucasdias.ld10@gmail.com>
2023-03-09 10:45:16 -0300

    partial progress in like posts

--- src/repositories/posts.repository.js
+++ src/repositories/posts.repository.js
@@ -25,3 +25,13 @@ export async function deletePostQuery(post) {
 
     return result
 }
+
+export async function likePostQuery(post) {
+        const like = await db.query(`SELECT like_count FROM posts WHERE id = $1`, [post.id]);
+
+        const newLikes = like.rows[0].like_count + 1
+
+        const updateLikes = await db.query(`UPDATE posts SET like_count = $1 WHERE id = $2`, [newLikes, post.id])
+
+        return updateLikes
+}
--- src/routers/posts.router.js
+++ src/routers/posts.router.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import createPost from '../controller/createPost.controller.js';
 import getPosts from '../controller/getPosts.controller.js';
-import { deletePost, editPost } from '../controller/posts.controller.js';
+import { deletePost, editPost, likePost } from '../controller/posts.controller.js';
 const postsRouter = express.Router();
 
 postsRouter.get('/', getPosts);
 postsRouter.post('/', createPost);
 postsRouter.patch('/', editPost);
 postsRouter.delete('/', deletePost);
+postsRouter.post('/like', likePost);
 
 export default postsRouter;
