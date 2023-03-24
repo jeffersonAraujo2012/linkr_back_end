92c92962a7d7f689630bd3f7bb70f6e40b76652a
luanmed <luanmede@gmail.com>
2023-03-24 11:26:02 -0300

    fix: queries get post

--- src/repositories/search.repository.js
+++ src/repositories/search.repository.js
@@ -15,14 +15,45 @@ export async function searchUserByName (id, name) {
 }
 
 export async function searchUserById (id) {
-    const user = await db.query(`
-        SELECT posts.*, users.username, users.picture_url AS picture_user
-        FROM posts
-        RIGHT JOIN users
-        ON posts.user_id = users.id
-	    WHERE users.id = $1
-        ORDER BY created_at DESC
-        LIMIT 20;`, [id]);
+    const user = await db.query(
+        // `
+        // SELECT posts.*, users.username, users.picture_url AS picture_user
+        // FROM posts
+        // RIGHT JOIN users
+        // ON posts.user_id = users.id
+	    // WHERE users.id = $1
+        // ORDER BY created_at DESC
+        // LIMIT 20;`, [id]);
+
+        `SELECT 
+        posts.*, 
+        users.username, 
+        users.picture_url AS picture_user, 
+        COUNT(likes.id) AS likes_count, 
+        array_agg(liked_users.id) AS liked_by
+        FROM 
+        posts
+        RIGHT JOIN users ON posts.user_id = users.id
+        LEFT JOIN likes ON posts.id = likes.post_id
+        LEFT JOIN (
+            SELECT 
+                likes.post_id, 
+                users.id 
+            FROM 
+                likes 
+                JOIN users ON likes.user_id = users.id
+        ) liked_users ON posts.id = liked_users.post_id
+        WHERE 
+        users.id = $1
+        GROUP BY 
+        posts.id, 
+        users.username, 
+        users.id, 
+        picture_user
+        ORDER BY 
+        posts.created_at DESC
+        LIMIT 20;    
+    `, [id]);
 
     return user;
 }
