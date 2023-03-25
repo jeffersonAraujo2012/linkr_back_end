b0765dcdcefc19af9a8d368d7c65fba47a5093bf
Jefferson Araújo <jeffersonc201212@gmail.com>
2023-03-24 21:03:02 -0300

    Fixed: get posts query.

--- src/repositories/getPosts.repository.js
+++ src/repositories/getPosts.repository.js
@@ -45,7 +45,7 @@ export async function getPostsQuery(id) {
     //`
 
     `SELECT posts.*, users.username, users.picture_url AS picture_user, COUNT(likes.id) AS likes_count,
-    array_agg(CASE WHEN likes.post_id = posts.id THEN likes.user_id ELSE NULL END) 
+    array_agg(CASE WHEN likes.post_id = posts.id THEN likes.user_id ELSE NULL END) AS liked_by
     FROM posts 
     LEFT JOIN users 
     ON users.id = posts.user_id
