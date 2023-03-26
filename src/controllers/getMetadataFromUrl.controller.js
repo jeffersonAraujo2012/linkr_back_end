a8cf2c64e84deaff15cac761f54dfcd13d64c99c
Jefferson Araújo <jeffersonc201212@gmail.com>
2023-03-26 19:16:30 -0300

    Attempt to make evaluator accept login persistence

--- src/controllers/getMetadataFromUrl.controller.js
+++ src/controllers/getMetadataFromUrl.controller.js
@@ -1,12 +1,15 @@
 import urlMetadata from "url-metadata";
 
 export default async function getMetadataFromUrl(req, res) {
-  const url = req.query.url;
+  let url = req.query.url;
+  if (url[url.length - 1] === '/') {
+    url = url.slice(0,-1);
+  }
 
   try {
-    const data = await urlMetadata(url);
+    const data = await urlMetadata(url, {timeout: 60000});
     res.status(200).send(data);
   } catch (error) {
-    res.status(500).send("Internal Error");
+    res.status(500).send(error);
   }
 }
